26621de208e361c4d655ee37c885c8dc8ec1846c
ryan-saragih <ryansaragih2502@gmail.com>
2013-12-16 23:14:07 +0700

    update PHP

---
+++ proses_sign_up.php
@@ -0,0 +1,20 @@
+<?
+include "koneksi.php";
+
+$nama=$_POST['User'];
+$pass=$_POST['password'];
+$email=$_POST['email'];
+
+
+if($nama == ""){
+	
+	echo"<script>alert('Kosong cuy',document.location.href='sign_up.html')</script>
+	";}
+else {
+	
+$sa=mysql_query("insert into sign_up values ('$_POST[User]','$_POST[password]','$_POST[email]')");
+
+echo "<script>alert('berhasil disimpan',document.location.href='Main.html')</script>
+";
+}
+?>
